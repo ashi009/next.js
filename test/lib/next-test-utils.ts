0be56462b662d2848e6664daf7043da65d5d7e15
Jiachi Liu <inbox@huozhi.im>
2025-02-09 21:47:39 +0100

    fix query

--- test/lib/next-test-utils.ts
+++ test/lib/next-test-utils.ts
@@ -989,8 +989,10 @@ function getRedboxFloatingHeaderText(
       .find((p) =>
         p.shadowRoot.querySelector('[data-nextjs-dialog-header-total-count]')
       )
-    const root = portal.shadowRoot
-    return root?.innerText
+
+    const root = portal?.shadowRoot
+    return root?.querySelector('[data-nextjs-dialog-header-total-count]')
+      ?.innerText
   })
 }
 
